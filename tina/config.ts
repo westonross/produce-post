f51896febf64a76cdc771a426a244dc79c6f97cf
westonross <westonross@outlook.com>
2025-01-07 14:31:30 -0500

    tokens

--- tina/config.ts
+++ tina/config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tinacms";
 export default defineConfig({
   branch: "master",
   clientId: "b9f2e0d0-e9d6-4479-81cc-7309d0e59d8f",  // Your client ID from dashboard
-  token: "7ab8c9411eafc24a65573a9636ad0c258d97365d",  // Your GitHub Action token
+  token: "820c1c73d752a2a155967de7714f26c584963100",  // Your GitHub Action token
   basePath: "/produce-post",  // For GitHub Pages
   build: {
     outputFolder: "docs/admin",
