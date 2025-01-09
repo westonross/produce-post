1b8184aa6f2cb5796b7e16c08956c6ae3cca2ddd
westonross <westonross@outlook.com>
2025-01-09 18:24:18 -0500

    asdf

--- tina/config.ts
+++ tina/config.ts
@@ -65,7 +65,7 @@ export default defineConfig({
             label: "Image Credit",
           },
           {
-            type: "string",
+            type: "datetime",
             name: "image_date",
             label: "Image Date",
           },
